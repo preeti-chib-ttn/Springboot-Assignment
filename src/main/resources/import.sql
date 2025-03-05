836d51e0e32978845737115afdb8d4ca2a0e9680
Preeti Chib <preeti.chib@tothenew.com>
2025-03-05 12:45:24 +0530

    restart autogenerated value with 4 to make api working

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,4 @@
 INSERT INTO employee (id, employee_name, employee_designation) VALUES (1, 'Preeti Chib', 'Software Engineer');
 INSERT INTO employee (id, employee_name, employee_designation) VALUES (2, 'Prashant Gupta', 'Product Manager');
 INSERT INTO employee (id, employee_name, employee_designation) VALUES (3, 'Jagriti Mishra', 'DevOps Engineer');
+ALTER TABLE employee ALTER COLUMN id RESTART WITH 4;
